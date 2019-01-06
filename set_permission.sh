5691aa6a38fabd4819504e7aeaf6d0f1fee82f64
YA <YA-androidapp@users.noreply.github.com>
2019-01-07 06:17:24 +0900

    Create set_permission.sh

---
+++ set_permission.sh
@@ -0,0 +1,5 @@
+﻿#!/bin/sh
+sudo chown -R h2z:www-data /path/to/Laravel-Isbn
+sudo find /path/to/Laravel-Isbn -type d -exec chmod 750 {} \;
+sudo find /path/to/Laravel-Isbn -type f -exec chmod 640 {} \;
+sudo chmod -R 770 /path/to/Laravel-Isbn/storage/ /path/to/Laravel-Isbn/bootstrap/cache/
