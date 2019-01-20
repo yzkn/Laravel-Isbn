074e6f0537e5c5e46250f88fa2edf73f7a3f342d
YA <YA-androidapp@users.noreply.github.com>
2019-01-21 00:53:16 +0900

    Sort by "series"

--- public/js/sort.js
+++ public/js/sort.js
@@ -1,16 +1,35 @@
 document.addEventListener("DOMContentLoaded", function (event) {
-    var options = {
-        valueNames: ["title", "pubdate", "author", "publisher"]
-    };
-    var bookList = new Listjs("books", options);
-    if (bookList) {
-        bookList.sort("title", {
-            order: "asc",
-            alphabet: "AＡaａBＢbｂCＣcｃDＤdｄEＥeｅ" +
-                "FＦfｆGＧgｇHＨhｈIＩiｉJＪjｊ" +
-                "KＫkｋLＬlｌMＭmｍNＮnｎOＯoｏ" +
-                "PＰpｐQＱqｑRＲrｒSＳsｓTＴtｔ" +
-                "UＵuｕVＶvｖWwＷｗXＸxｘYＹyｙZＺzｚ"
-        });
+    if (document.getElementById("books")) {
+        var options = {
+            valueNames: ["title", "pubdate", "author", "publisher"]
+        };
+        var bookList = new Listjs("books", options);
+        if (bookList) {
+            bookList.sort("title", {
+                order: "asc",
+                alphabet: "AＡaａBＢbｂCＣcｃDＤdｄEＥeｅ" +
+                    "FＦfｆGＧgｇHＨhｈIＩiｉJＪjｊ" +
+                    "KＫkｋLＬlｌMＭmｍNＮnｎOＯoｏ" +
+                    "PＰpｐQＱqｑRＲrｒSＳsｓTＴtｔ" +
+                    "UＵuｕVＶvｖWwＷｗXＸxｘYＹyｙZＺzｚ"
+            });
+        }
+    }
+
+    if (document.getElementById("series")) {
+        var options = {
+            valueNames: ["series"]
+        };
+        var seriesList = new Listjs("series", options);
+        if (seriesList) {
+            seriesList.sort("series", {
+                order: "asc",
+                alphabet: "AＡaａBＢbｂCＣcｃDＤdｄEＥeｅ" +
+                    "FＦfｆGＧgｇHＨhｈIＩiｉJＪjｊ" +
+                    "KＫkｋLＬlｌMＭmｍNＮnｎOＯoｏ" +
+                    "PＰpｐQＱqｑRＲrｒSＳsｓTＴtｔ" +
+                    "UＵuｕVＶvｖWwＷｗXＸxｘYＹyｙZＺzｚ"
+            });
+        }
     }
 });
--- resources/views/book/series.blade.php
+++ resources/views/book/series.blade.php
@@ -7,43 +7,52 @@
 <div class="container">
     <h1>{{ $title }}</h1>
     <div class="table-responsive">
-        <table class="table table-striped">
-            <thead>
-                <tr>
-                    <th>{{ __('Series') }} <br /> {{ __('Publisher') }} <br /> {{ __('Author') }}</th>
-                    <th colspan="{{ $colspan }}">{{ __('Volume') }} <br /> {{ __('Pubdate') }}</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($books_groupby_series as $ser => $books_in_series)
-                    @php
-                        $i = 0;
-                    @endphp
+        <div id="series">
+            <table class="table table-striped">
+                <thead>
                     <tr>
-                        @foreach ($books_in_series as $vol => $book)
-                            @if ($i===0)
-                                <th>{{ $ser }} <br /> {{ $book->summary__publisher }} <br /> {{ $book->summary__author }}</th>
-                            @endif
-                            <td>
-                                <a href="{{ url('books/'.$book->id) }}">
-                                    <b>
-                                        {{ $vol }}
-                                    </b>
-                                    @if ($isOnline && !empty($book->summary__cover) && exif_imagetype($book->summary__cover))
-                                        <img src="{{ $book->summary__cover }}" class="img-thumbnail summary__cover" />
-                                    @endif
-                                    <br />
-                                    {{ $book->summary__pubdate }}
-                                </a>
-                            </td>
-                            @php
-                                $i++;
-                            @endphp
-                        @endforeach
+                        <th colspan="3">
+                            <input type="text" class="form-control search" placeholder="Search" />
+                        </th>
                     </tr>
-                @endforeach
-            </tbody>
-        </table>
+                    <tr>
+                        <th class="sort" data-sort="series">{{ __('Series') }}</th>
+                        <th>{{ __('Publisher') }} <br /> {{ __('Author') }}</th>
+                        <th colspan="{{ $colspan }}">{{ __('Volume') }} <br /> {{ __('Pubdate') }}</th>
+                    </tr>
+                </thead>
+                <tbody class="list">
+                    @foreach ($books_groupby_series as $ser => $books_in_series)
+                        @php
+                            $i = 0;
+                        @endphp
+                        <tr>
+                            @foreach ($books_in_series as $vol => $book)
+                                @if ($i===0)
+                                    <td class="series">{{ $ser }}</td>
+                                    <td>{{ $book->summary__publisher }} <br /> {{ $book->summary__author }}</td>
+                                @endif
+                                <td>
+                                    <a href="{{ url('books/'.$book->id) }}">
+                                        <b>
+                                            {{ $vol }}
+                                        </b>
+                                        @if ($isOnline && !empty($book->summary__cover) && exif_imagetype($book->summary__cover))
+                                            <img src="{{ $book->summary__cover }}" class="img-thumbnail summary__cover" />
+                                        @endif
+                                        <br />
+                                        {{ $book->summary__pubdate }}
+                                    </a>
+                                </td>
+                                @php
+                                    $i++;
+                                @endphp
+                            @endforeach
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
     </div>
 </div>
 @endsection
