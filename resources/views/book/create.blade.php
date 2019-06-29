8c20abe79991336bf7f2078f5f673162425590cc
YA <YA-androidapp@users.noreply.github.com>
2019-06-29 13:56:18 +0900

    Remove barcode reader

--- resources/views/book/create.blade.php
+++ resources/views/book/create.blade.php
@@ -42,7 +42,6 @@
                         <div class="card">
                             <div class="card-body">
                                 <dl class="row">
-                                    @component('components.barcode.reader')
                                     @endcomponent
                                     <dt class="col-md-2">{{ __('ISBN') }}</dt>
                                     <dd class="col-md-10"><input type="text" class="form-control" name="summary__isbn" id="summary__isbn" value="{{ $isbn }}"></dd>
